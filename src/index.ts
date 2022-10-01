ae3f35379cf955a8c4f9dd53153d1741c7455e01
Floris Porro <floris@streammyevent.com>
2022-10-01 11:12:40 +0200

    Add websocket

--- src/index.ts
+++ src/index.ts
@@ -2,52 +2,44 @@ export { Tracker } from './lib/tracker'
 
 export default {
   async fetch(request: Request, env: Env) {
-    try {
+    return await handleErrors(request, async () => {
       const url = new URL(request.url);
       const path = url.pathname.slice(1).split('/');
+      const name = path[0];
+  
+      if (!name) {
+        return new Response("Invalid request", { status: 400 })
+      }
       
-      return await handleRequest(request, env, path)
-    } catch (e) {
-      return new Response(`${e}`)
-    }
+      const trackerId = env.TRACKER.idFromName(name)
+      const trackerObj = env.TRACKER.get(trackerId)
+
+      return trackerObj.fetch(request)
+    })
   },
 }
 
-async function handleRequest(request: Request, env: Env, path: Array<string>) {
-  const name = path[0];
-  
-  if (!name) {
-    return new Response("Invalid request", { status: 400 })
-  }
-  
-  if (name === "ws") {
-    const upgradeHeader = request.headers.get('Upgrade');
-    if (!upgradeHeader || upgradeHeader !== 'websocket') {
-      return new Response('Expected Upgrade: websocket', { status: 426 });
+async function handleErrors(request: Request, func: Function) {
+  try {
+    return await func(request)
+  } catch (e: any) {
+
+    if (request.headers.get("Upgrade") == "websocket") {
+      const webSocketPair = new WebSocketPair();
+      const [client, server] = Object.values(webSocketPair);
+
+      server.accept();
+      server.send(JSON.stringify({
+        error: e.stack
+      }));
+      server.close(1011, "Uncaught exception during session setup")
+      return new Response(null, { status: 101, webSocket: client });
+
+    } else {
+      return new Response(e.stack, { status: 500 })
     }
-    
-    const webSocketPair = new WebSocketPair();
-    const [client, server] = Object.values(webSocketPair);
-    
-    server.accept();
-    
-    server.addEventListener('message', event => {
-      console.log(event.data);
-    });
-
-    return new Response(null, {
-      status: 101,
-      webSocket: client,
-    });
   }
-  
-  const trackerId = env.TRACKER.idFromName(name)
-  const trackerObj = env.TRACKER.get(trackerId)
-  const response = await trackerObj.fetch(request)
-  console.log(response)
-  return response
 }
-
 interface Env {
   TRACKER: DurableObjectNamespace
 }
--- src/lib/tracker.ts
+++ src/lib/tracker.ts
@@ -7,79 +7,59 @@ interface Env {}
 
 export class Tracker {
   state: DurableObjectState
-  name?: string;
-	type?: string;
-	frames: Array<Frame>
-	meta?: any;
+  storage: DurableObjectStorage | undefined
+  sessions: Array<any>
 
   constructor(state: DurableObjectState, env: Env) {
     this.state = state;
-    this.frames = []
-
-    this.state.blockConcurrencyWhile(async () => {
-      this.name = await this.state.storage?.get("name") || "";
-      this.type = await this.state.storage?.get("type") || "";
-      this.meta = await this.state.storage?.get("meta") || {};
-      this.frames = await this.state.storage?.get("frames") || [];
-    });
+    this.storage = state.storage;
+    this.sessions = []
   }
 
-  async handleRecord(newFrame: any) {
-    function isNewFrame(payload: any): payload is NewFrame {
-      return payload.position !== undefined && payload.position.latitude !== undefined && payload.position.longitude !== undefined;
-    }
-
-    if (!isNewFrame(newFrame)) {
-      return new Response("Invalid payload", { status: 400 })
-    }
-  
-    let currentFrame
-    if (this.frames && this.frames.length > 0) {
-      currentFrame = this.frames[this.frames.length - 1]
-    }
-
-    if (currentFrame?.positionTimestamp !== undefined) {
-      if (currentFrame?.positionTimestamp === newFrame.positionTimestamp) {
-        return new Response("Frame already exists", {
-          status: 400
-        })
-      }
-    }
-
-    const frame = new Frame({ ...newFrame, lastFrame: currentFrame })
-    this.frames.push(frame)
-    await this.state.storage?.put("frames", this.frames);
-
-    const json = JSON.stringify(frame, null, 2)
-    return new Response(json, { status: 200 })
-  
+  async getFrames() {
+    const response = await this.state.storage?.list({ reverse: true, limit: 500 }) || []
+    const frames = [...response.values()]
+    const json = JSON.stringify(frames, null, 2)
+    return json
   }
 
-  // Handle HTTP requests from clients.
   async fetch(request: Request) {
-    // Apply requested action.
-    let url = new URL(request.url);
+    const url = new URL(request.url);
     const path = url.pathname.slice(1).split('/').slice(1).join('/');
 		const method = request?.method?.toUpperCase?.();
 
-    console.log(path);
-
+    console.log(path)
 
 		if (method === "GET") {
-      const tracker = {
-        name: this.name,
-        type: this.type,
-        meta: this.meta,
-        frames: this.frames
+      if (path === "ws") {
+        const upgradeHeader = request.headers.get('Upgrade');
+        if (!upgradeHeader || upgradeHeader !== 'websocket') {
+          return new Response('Expected Upgrade: websocket', { status: 426 });
+        }
+        
+        const webSocketPair = new WebSocketPair();
+        const [client, server] = Object.values(webSocketPair);
+        
+        await this.handleSession(server)
+        
+        server.addEventListener('message', event => {
+          console.log(event.data);
+        });
+
+        return new Response(null, {
+          status: 101,
+          webSocket: client,
+        });
       }
 
-      const json = JSON.stringify(tracker, null, 2)
-
+      const json = await this.getFrames()
+  
       return new Response(json, {
         headers: {
           "content-type": "application/json;charset=UTF-8"
         }
       });
+
 		}
 
 		if (method === "POST") {
@@ -88,19 +68,19 @@ export class Tracker {
       console.log(json)
 
       switch (path) {
-        case "setmeta":
-          break;
         case "record/basic":
+          console.log("Storing basic frame")
           return await this.handleRecord(json);
-        case "record/lightbug":
+          case "record/lightbug":
+          console.log("Handling Lightbug frame")
           if (json.SubscribeURL !== undefined) {
+            console.log("Subscribing to Lightbug feed")
             await fetch(json.SubscribeURL);
             return new Response("subscribed", {
               status: 200
             });
           }
           const frame = lightbugPayloadToFrame(json)
-          console.log(frame)
           return await this.handleRecord(frame)
         default:
           return new Response("nope", {
@@ -113,5 +93,70 @@ export class Tracker {
     return new Response("nope", {
       status: 418
   });
+  }
+
+  async handleSession(webSocket: WebSocket) {
+    webSocket.accept();
+    let session = { webSocket }
+    this.sessions.push(session)
+
+    const json = await this.getFrames()
+    webSocket.send(json)
+
+    const closeOrErrorHandler = (event: any) => {
+      console.log("Closing session")
+      this.sessions.filter(member => member !== session)
+    }
+
+    webSocket.addEventListener('close', closeOrErrorHandler);
+    // @ts-ignore
+    webSocket.addEventListener("error", closeOrErrorHandler);
+
+    return
+  }
+
+  broadcast(message: any) {
+    if (typeof message !== "string") {
+      message = JSON.stringify(message);
+    }
+
+    this.sessions.forEach(session => {
+      try {
+        session.webSocket.send(message);
+      } catch(e) {
+        // Probably dead connection
+        console.log(e)
+        this.sessions.filter(member => member !== session)
+      }
+    })
+  }
+
+  async handleRecord(newFrame: any) {
+    function isNewFrame(payload: any): payload is NewFrame {
+      return payload.position !== undefined && payload.position.latitude !== undefined && payload.position.longitude !== undefined;
+    }
+
+    if (!isNewFrame(newFrame)) {
+      return new Response("Invalid payload", { status: 400 })
+    }
+
+    const storage = await this.storage?.list({ reverse: true, limit: 1000 }) || []
+    const frames = [...storage?.values()]
+
+    const currentFrame = frames[0] as Frame
+
+    const frame = new Frame({ ...newFrame, lastFrame: currentFrame })
+
+    const time = frame.positionTimestamp || frame.frameTimestamp
+    const key = new Date(time).toISOString()
+    const json = JSON.stringify(frame)
+
+    this.broadcast(json)
+    console.log(json)
+
+    await this.storage?.put(key, json);
+
+    return new Response(json, { status: 200 })
+  
   }
 }
